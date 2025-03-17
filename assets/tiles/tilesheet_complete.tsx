421e0940f772fda87f370082ba0b51db7e7411c0
Alec.Uy <meep130@gmail.com>
2025-03-17 21:30:47 +0800

    Fix map rendering issue

--- assets/tiles/tilesheet_complete.tsx
+++ assets/tiles/tilesheet_complete.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tilesheet_complete" tilewidth="64" tileheight="64" tilecount="264" columns="22">
- <image source="/tilesheet_complete.png" width="1408" height="768"/>
+ <image source="tilesheet_complete.png" width="1408" height="768"/>
 </tileset>
 
