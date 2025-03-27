f3dbe96601a67fb96abe2689210a2953097c4839
Alec.Uy <meep130@gmail.com>
2025-03-28 01:45:30 +0800

    Make red button bigger

--- lib/screens/minigame_screen.dart
+++ lib/screens/minigame_screen.dart
@@ -222,7 +222,7 @@ class _MiniGameScreenState extends State<MiniGameScreen> {
                             child: Image.asset(
                               'assets/images/red_button.png',
                               width: 100,
-                              height: 50,
+                              height: 150,
                             ),
                           ),
                         ],
